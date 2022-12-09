0be6e16b59585ff260c610b069e4ecb2aae197da
Henry Will <99243374+henry-will@users.noreply.github.com>
2022-12-09 10:02:20 +0900

    Add typescript-axios generator

---
+++ typescript-axios/generate-typescript.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+CURRENT_FILE_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+PROJECT_DIR=$(cd "$CURRENT_FILE_DIR" && cd .. && pwd )
+cd "${CURRENT_FILE_DIR}"
+"${PROJECT_DIR}"/bin/openapi-generator-cli generate -c "${CURRENT_FILE_DIR}/typescript-klay-api-config.yaml"
+"${PROJECT_DIR}"/bin/openapi-generator-cli generate -c "${CURRENT_FILE_DIR}/typescript-governance-api-config.yaml"

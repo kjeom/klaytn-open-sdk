ff6f54c1be037b41cff541ce72e0431c46c9cf05
kjeom <jack.jin@klaytn.foundation>
2023-03-20 19:31:38 +0900

    refactor python

--- sdk/client/python/openapi-test/test/klay/KlayApiGetRewardsTest.py
+++ sdk/client/python/openapi-test/test/klay/KlayApiGetRewardsTest.py
@@ -6,10 +6,12 @@ from caver_python.apis.tags.klay_api import KlayApi
 
 class TestKlayGetRewards(unittest.TestCase):
     host="http://localhost:8551"
+    # host="https://api.baobab.klaytn.net:8651"
     klay = KlayApi(ApiClient(configuration=Configuration(host=host)))
     
     def test_post(self):
-        result = self.klay.get_rewards(1.0)
+        result = self.klay.get_rewards(1)
+        print(result)
 
 if __name__ == '__main__':
     unittest.main()
